d5c46db198d1e249705a579df7265475dd26276e
Daniel Riding <daniel.riding@rizepoint.com>
2017-10-31 16:28:46 -0600

    fixed added routes for dashboard and heroes

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Hero } from "./hero";
 import { HeroService } from "./hero.service";
@@ -11,4 +11,11 @@ import { HeroService } from "./hero.service";
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+    .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+  
 }
