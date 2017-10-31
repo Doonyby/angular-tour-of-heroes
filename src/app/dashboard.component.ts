828b0ca932f9df2022a624f67ee0914723d4dd41
Dan Riding <doonyby@gmail.com>
2017-10-30 23:06:47 -0600

    added first routes to heroes and dashboard

---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
+
+@Component({
+  selector: "my-dashboard",
+  templateUrl: "./dashboard.component.html",
+})
+
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+}
